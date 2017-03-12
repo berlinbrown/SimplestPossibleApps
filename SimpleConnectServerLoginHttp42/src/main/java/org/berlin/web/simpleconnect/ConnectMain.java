6ce0939e5e984360fa84f4f6339fb1b15c8bce3a
berlinbrown <berlinbrown@github.com>
2017-03-12 18:33:05 -0400

    More updates, adding initial pom.xml

---
+++ SimpleConnectServerLoginHttp42/src/main/java/org/berlin/web/simpleconnect/ConnectMain.java
@@ -0,0 +1,10 @@
+/**
+ * Open Source
+ * Berlin
+ */
+public class ConnectMain {
+  public static void main(final String [] args) {
+     System.out.println("Running");
+     System.out.println("Done");
+  }
+} 
